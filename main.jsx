7162c9bef6eb77b1325edee9d7d6aa25e0feb565
Zacharia316 <zappyblues234@gmail.com>
2026-05-05 21:11:12 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LuminarMusicStudio from './LuminarMusicStudio.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LuminarMusicStudio />
+  </React.StrictMode>
+)
